061c167508b6eea1df8884a687ae3b933cd6c81b
Erin Green <erin.y.green@gmail.com>
2018-12-02 18:22:43 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,10 +6,4 @@ class Character < ActiveRecord::Base
     "#{self.name} always says: #{self.catchphrase}"
   end
   
- # def build_show(name: name)
-    #new_show = Show.new(name: "#{name}")
-   # self.show = new_show 
-  #end
-  
-  #jules_cobb.build_show(:name => "Cougar Town")
 end
