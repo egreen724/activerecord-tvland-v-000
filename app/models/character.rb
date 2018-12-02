dca611ca0c6529f7276cf194ef0881838ca14496
Erin Green <erin.y.green@gmail.com>
2018-12-02 18:25:36 +0000

    submitting

--- app/models/character.rb
+++ app/models/character.rb
@@ -6,4 +6,13 @@ class Character < ActiveRecord::Base
     "#{self.name} always says: #{self.catchphrase}"
   end
   
+<<<<<<< HEAD
+=======
+  def build_show(name: name)
+    new_show = Show.new(name: "#{name}")
+    self.show = new_show 
+  end
+  
+  #jules_cobb.build_show(:name => "Cougar Town")
+>>>>>>> bd942f4fa163100111b6342ae81813d518159038
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -3,5 +3,12 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   
+<<<<<<< HEAD
+=======
+  def build_network(call_letters: letters)
+    new_network = Network.new(:call_letters => "#{letters}")
+    self.network = new_network
+  end
+>>>>>>> bd942f4fa163100111b6342ae81813d518159038
   
 end
